933ea8fc6adb48cd8b9449b8d5842d8537cdf4da
sadhanaagasthya <107806715+sadhanaagasthya@users.noreply.github.com>
2022-07-25 16:25:12 +0530

    Add files via upload

---
+++ balancebracket/main/Driver1.java
@@ -0,0 +1,20 @@
+package com.gl.balancebracket.main;
+
+import com.gl.balancebracket.service.BalancingBrackets;
+
+public class Driver1 {
+	public static void main(String[] args) {
+		
+		BalancingBrackets balancingBrackets= new BalancingBrackets();
+		String expression="([[{}]])";
+		
+		Boolean result;
+		
+		result= balancingBrackets.checkingBracketsBalanced(expression);
+		if(result)
+			System.out.println("The entered string has balanced brackets");
+		else
+			System.out.println("The entered string do not contain balanced brackets");
+	}
+
+}
